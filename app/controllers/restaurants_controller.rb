039f245e5f63c9736b706fa1851bb75d2b9d5fac
latinman <erivas78@gmail.com>
2015-09-04 17:34:24 -0400

    Updated new review form with clickable star ratings

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -23,6 +23,11 @@ class RestaurantsController < ApplicationController
 
   def show
     @reviews = Review.where(restaurant_id: @restaurant.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
 
   def edit
